d29bc8163743600fdc5da8f82eb1f89e136a3db4
praneeth <gp.gunturu@gmail.com>
2025-04-16 18:31:32 -0500

    Wrapped the logout button with Form in the MainNavigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, {action as authAction} from './pages/Authentication';
+import { action as logoutAction } from './pages/Logout';
 
 const router = createBrowserRouter([
   {
@@ -60,7 +61,8 @@ const router = createBrowserRouter([
         element: <NewsletterPage />,
         action: newsletterAction,
       },
-      {path: 'auth', element: <AuthenticationPage />, action: authAction}
+      {path: 'auth', element: <AuthenticationPage />, action: authAction},
+      {path: 'logout', action: logoutAction}
     ],
   },
 ]);
